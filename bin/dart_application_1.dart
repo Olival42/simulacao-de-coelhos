0adcfd8b20f7ef7ffcf06ecbd778861d9e42f63d
Pedro Henrique Olival <166179904+Olival42@users.noreply.github.com>
2025-08-26 21:09:39 -0300

    Update dart_application_1.dart

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,34 +1,37 @@
 void main() {
-  int adultos = 2;
-  int jovens = 0;
+  int adultos = 0;
+  int jovens = 2;
+  int mes = 1;
 
-  print("Simulação sem predador:");
+  print("Simulação:");
 
-  for (var i = 1; i <= 12; i++) {
+  while (adultos + jovens > 0) {
     int novosAdultos = jovens;
     adultos += novosAdultos;
     jovens = adultos;
     int total = adultos + jovens;
 
-    print("Fim do Mês: $i. População de coelhos: $total. Jovens: $jovens. Adultos: $adultos");
-  }
+    if (mes > 12) {
+      total = (total * 0.25).toInt();
 
-  print("\nSimulação com predador:");
+      if (total == 1) {
+        total = 0;
+        jovens = 0;
+        adultos = 0;
+      } else {
+        jovens = (total / 2).toInt();
+        adultos = (total / 2).toInt();
+      }
+    }
 
-  int mes = 1;
-  while (jovens + adultos > 0) {
-    int total = jovens + adultos;
-
-    total = (total * 0.25).toInt();
-    jovens = (total / 2).toInt();
-    adultos = (total / 2).toInt();
-
-    print("Fim do Mês: $mes. População de coelhos: $total. Jovens: $jovens. Adultos: $adultos");
+    print(
+      "Fim do Mês: $mes. População de coelhos: $total. Jovens: $jovens. Adultos: $adultos",
+    );
 
     mes++;
 
     if (total == 0) {
-      print("Todos os coelhoes foram predados.");
+      print("Todos os coelhos foram predados.");
       break;
     }
   }
